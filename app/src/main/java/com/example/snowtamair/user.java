3a5d4d82dcf27cef27997828c6832e9752938a3c
Unknown <albane.gril@free.fr>
2019-11-20 11:56:33 +0100

    set user swip menu

--- app/src/main/java/com/example/snowtamair/user.java
+++ app/src/main/java/com/example/snowtamair/user.java
@@ -31,14 +31,14 @@ public class user extends AppCompatActivity {
         setContentView(R.layout.activity_user);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
