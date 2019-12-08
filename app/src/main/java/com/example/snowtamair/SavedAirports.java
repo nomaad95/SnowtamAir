1025fdb8e5acffa6e43d52fbb7a685cb60dd6ab4
Camille <camille.bamboute@hotmail.fr>
2019-12-08 18:21:52 +0100

    addAirportToList

--- app/src/main/java/com/example/snowtamair/SavedAirports.java
+++ app/src/main/java/com/example/snowtamair/SavedAirports.java
@@ -9,13 +9,18 @@ public class SavedAirports {
 
     public void setListAirport(ArrayList<Airport> listAirport) { this.listAirport = listAirport; }
 
-    public void addAiportToList(Airport Airport){
+    public void addAiportToList(Airport airport){
         if(listAirport.size() < 4){
-            listAirport.add(Airport);
+            listAirport.add(airport);
         }
 
         else{
+            Airport temp = listAirport.get(listAirport.size()-1);
+            for (int i=0; i > 0; i--){
+                listAirport.set(i, listAirport.get(i-1));
+            }
 
+            listAirport.set(0, airport);
         }
 
     }
